c963cb835fe09141506df5e9ce1a09dcb96fe089
lseaJK <xiaoxin23001@126.com>
2025-08-05 21:41:01 +0800

    Remove TradingAgents-CN to convert to submodule

--- TradingAgents-CN/tradingagents/llm_adapters/__init__.py
+++
@@ -1,5 +0,0 @@
-# LLM Adapters for TradingAgents
-from .dashscope_adapter import ChatDashScope
-from .dashscope_openai_adapter import ChatDashScopeOpenAI
-
-__all__ = ["ChatDashScope", "ChatDashScopeOpenAI"]
