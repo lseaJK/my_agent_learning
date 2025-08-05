e6ffb57073a8791b18ea517f1166ce9c016ceca4
lseaJK <xiaoxin23001@126.com>
2025-08-05 21:24:21 +0800

    Remove FinGenius from index to convert to submodule

--- FinGenius/src/environment/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Environment module for different execution environments."""
-
-from src.environment.base import BaseEnvironment
-from src.environment.battle import BattleEnvironment
-from src.environment.research import ResearchEnvironment
-
-
-__all__ = ["BaseEnvironment", "ResearchEnvironment", "BattleEnvironment"]
